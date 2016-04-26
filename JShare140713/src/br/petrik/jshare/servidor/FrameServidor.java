1dc1082a64a6cdd91465cf2e6dc6bbd69c435685
jpspetrik <jpspetrik@WS-LAB01_149.univel.univel.br>
2016-04-25 22:11:32 -0300

    .

--- JShare140713/src/br/petrik/jshare/servidor/FrameServidor.java
+++ JShare140713/src/br/petrik/jshare/servidor/FrameServidor.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class FrameServidor extends JFrame {
 
@@ -35,8 +40,24 @@ public class FrameServidor extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setForeground(new Color(50, 205, 50));
+		textArea.setFont(new Font("Monospaced", Font.BOLD, 16));
+		textArea.setEnabled(false);
+		textArea.setBackground(Color.BLACK);
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		contentPane.add(textArea, gbc_textArea);
 	}
 
 }
