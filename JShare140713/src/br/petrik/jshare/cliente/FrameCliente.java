c6d4488f3270630e46028e82e59d5523a4d5f421
JuanPetrik <juanpablopetrik@gmail.com>
2016-04-26 19:28:38 -0300

    Alterações

--- JShare140713/src/br/petrik/jshare/cliente/FrameCliente.java
+++ JShare140713/src/br/petrik/jshare/cliente/FrameCliente.java
@@ -1,28 +1,42 @@
 package br.petrik.jshare.cliente;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
-public class FrameCliente extends JFrame {
+import br.petrik.jshare.comum.interfaces.Cliente;
+import br.petrik.jshare.comum.interfaces.IServer;
+import br.petrik.jshare.comum.pojos.Arquivo;
+
+public class FrameCliente extends JFrame implements IServer{
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
 	private JTable table;
-	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -45,7 +59,7 @@ public class FrameCliente extends JFrame {
 	 */
 	public FrameCliente() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 481, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -73,14 +87,14 @@ public class FrameCliente extends JFrame {
 		gbc_lblNome.gridy = 1;
 		contentPane.add(lblNome, gbc_lblNome);
 		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 1;
-		contentPane.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
+		txtNome = new JTextField();
+		GridBagConstraints gbc_txtNome = new GridBagConstraints();
+		gbc_txtNome.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNome.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNome.gridx = 1;
+		gbc_txtNome.gridy = 1;
+		contentPane.add(txtNome, gbc_txtNome);
+		txtNome.setColumns(10);
 		
 		JLabel lblIp = new JLabel("IP Servidor");
 		GridBagConstraints gbc_lblIp = new GridBagConstraints();
@@ -90,14 +104,14 @@ public class FrameCliente extends JFrame {
 		gbc_lblIp.gridy = 2;
 		contentPane.add(lblIp, gbc_lblIp);
 		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 2;
-		contentPane.add(textField, gbc_textField);
-		textField.setColumns(10);
+		txtIP = new JTextField();
+		GridBagConstraints gbc_txtIP = new GridBagConstraints();
+		gbc_txtIP.insets = new Insets(0, 0, 5, 5);
+		gbc_txtIP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtIP.gridx = 1;
+		gbc_txtIP.gridy = 2;
+		contentPane.add(txtIP, gbc_txtIP);
+		txtIP.setColumns(10);
 		
 		JLabel lblPorta = new JLabel("Porta");
 		GridBagConstraints gbc_lblPorta = new GridBagConstraints();
@@ -107,14 +121,14 @@ public class FrameCliente extends JFrame {
 		gbc_lblPorta.gridy = 2;
 		contentPane.add(lblPorta, gbc_lblPorta);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_2.gridx = 3;
-		gbc_textField_2.gridy = 2;
-		contentPane.add(textField_2, gbc_textField_2);
+		txtPorta = new JTextField();
+		txtPorta.setColumns(10);
+		GridBagConstraints gbc_txtPorta = new GridBagConstraints();
+		gbc_txtPorta.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPorta.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPorta.gridx = 3;
+		gbc_txtPorta.gridy = 2;
+		contentPane.add(txtPorta, gbc_txtPorta);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
@@ -128,18 +142,169 @@ public class FrameCliente extends JFrame {
 		table = new JTable();
 		scrollPane.setColumnHeaderView(table);
 		
-		JButton btnNewButton = new JButton("Conectar");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 3;
-		gbc_btnNewButton.gridy = 4;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		btnConectar = new JButton("Conectar");
+		GridBagConstraints gbc_btnConectar = new GridBagConstraints();
+		gbc_btnConectar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnConectar.gridx = 3;
+		gbc_btnConectar.gridy = 4;
+		contentPane.add(btnConectar, gbc_btnConectar);
 		
-		JButton btnNewButton_1 = new JButton("Desconectar");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.gridx = 4;
-		gbc_btnNewButton_1.gridy = 4;
-		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+		btnDesconectar = new JButton("Desconectar");
+		GridBagConstraints gbc_btnDesconectar = new GridBagConstraints();
+		gbc_btnDesconectar.gridx = 4;
+		gbc_btnDesconectar.gridy = 4;
+		contentPane.add(btnDesconectar, gbc_btnDesconectar);
 	}
 
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy H:mm:ss:SSS");
+
+	private IServer servidor;
+	private Cliente cliente;
+	private Registry registry;
+	private String meunome;
+	private JButton btnConectar;
+	private JButton btnDesconectar;
+	private JTextField txtNome;
+	private JTextField txtIP;
+	private JTextField txtPorta;
+	
+	protected void configurar() {
+
+		btnConectar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				conectar();
+			}
+		});
+
+		btnDesconectar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				desconectar();
+			}
+		});
+
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				desconectar();
+			}
+		});
+	}
+
+	/**
+	 * Conecta no servidor.
+	 */
+	protected void conectar() {
+
+		meunome = txtNome.getText().trim();
+		if (meunome.length() == 0) {
+			JOptionPane.showMessageDialog(this, "Você precisa digitar um nome!");
+			return;
+		}
+
+		// Endereço IP
+		String host = txtIP.getText().trim();
+		if (!host.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+			JOptionPane.showMessageDialog(this, "O endereço ip parece inválido!");
+			return;
+		}
+
+		// Porta
+		String strPorta = txtPorta.getText().trim();
+		if (!strPorta.matches("[0-9]+") || strPorta.length() > 5) {
+			JOptionPane.showMessageDialog(this, "A porta deve ser um valor numérico de no máximo 5 dígitos!");
+			return;
+		}
+		int intPorta = Integer.parseInt(strPorta);
+
+		// Iniciando objetos para conexão.
+		try {
+			registry = LocateRegistry.getRegistry(host, intPorta);
+
+			servidor = (IServer) registry.lookup(IServer.NOME_SERVICO);
+			cliente = (Cliente) UnicastRemoteObject.exportObject(this, 0);
+
+			// Avisando o servidor que está entrando no Chat.
+			//servidor.entrarNoChat(meunome, cliente);
+
+			btnDesconectar.setEnabled(true);
+
+			btnConectar.setEnabled(false);
+			txtNome.setEnabled(false);
+			txtIP.setEnabled(false);
+			txtPorta.setEnabled(false);
+
+			btnConectar.setEnabled(false);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Desconecta do servidor.
+	 */
+	protected void desconectar() {
+		try {
+
+			if (servidor != null) {
+				//servidor.sair(meunome);
+				UnicastRemoteObject.unexportObject(this, true);
+
+				servidor = null;
+			}
+
+			//mostrar("Você saiu do chat.");
+
+			btnDesconectar.setEnabled(false);
+
+			btnConectar.setEnabled(true);
+			txtNome.setEnabled(true);
+			txtIP.setEnabled(true);
+			txtPorta.setEnabled(true);
+
+			btnConectar.setEnabled(true);
+
+			registry = null;
+			servidor = null;
+
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void registrarCliente(Cliente c) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<Cliente, List<Arquivo>> procurarArquivo(String nome) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] baixarArquivo(Arquivo arq) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void desconectar(Cliente c) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
 }
